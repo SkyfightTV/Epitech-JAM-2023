f88a7140621b80efcd5aa2291ab3bc99146fd1f9
SkyfightTV <antoine.berger@epitech.eu>
2023-02-11 14:23:11 +0100

    :sparkles: Add bat transformation

--- src/main/java/fr/dreamteam/jam/Main.java
+++ src/main/java/fr/dreamteam/jam/Main.java
@@ -3,5 +3,20 @@ package fr.dreamteam.jam;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    private static Main instance;
 
+    @Override
+    public void onEnable() {
+        instance = this;
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Goodbye World!");
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
 }
